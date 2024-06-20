4354319ef7968bbb6a1c92e6fdf7d34c539d4a49
vittorix <vittorix@gmail.com>
2024-06-20 18:18:14 -0400

    jagged arrays

--- ConsoleApp1/MultiDimensionArray.cs
+++ ConsoleApp1/MultiDimensionArray.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Utilities;
 
 public class MultiDimensionArray {
@@ -72,5 +73,35 @@ public class MultiDimensionArray {
         U.p($"{array3D.Length} equals {total}");
         // Output:
         // 12 equals 12
+
+        U.p("----- jagged arrays");
+        int[][] jaggedArray = new int[3][];
+        jaggedArray[0] = [1, 3, 5, 7, 9];
+        jaggedArray[1] = [0, 2, 4, 6];
+        jaggedArray[2] = [11, 22];
+
+        int[][] jaggedArray2 = 
+        [
+            [1, 3, 5, 7, 9],
+            [0, 2, 4, 6],
+            [11, 22]
+        ];
+
+        // Assign 77 to the second element ([1]) of the first array ([0]):
+        jaggedArray2[0][1] = 77;
+
+        // Assign 88 to the second element ([1]) of the third array ([2]):
+        jaggedArray2[2][1] = 88;
+
+        int[][,] jaggedArray3 =
+        [
+            new int[,] { {1,3}, {5,7} },
+            new int[,] { {0,2}, {4,6}, {8,10} },
+            new int[,] { {11,22}, {99,88}, {0,9} }
+        ];
+
+        Console.WriteLine("jaggedArray3[0][1, 0]: " + jaggedArray3[0][1, 0]);
+        Console.WriteLine("jaggedArray3.Rank: " + jaggedArray3.Rank);    
+        Console.WriteLine("jaggedArray3.Length: " + jaggedArray3.Length);    
     }
 }
